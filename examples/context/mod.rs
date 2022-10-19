23705f491abaa46d9f24202667dc655fa7c1ee14
moyy <412001799@qq.com>
2022-10-19 18:08:20 +0800

    feat: 调整 demo 代码，让 框架能跑起来。

--- examples/context/mod.rs
+++ examples/context/mod.rs
@@ -1,21 +1,45 @@
-
-
+use pi_render::{
+    components::view::{
+        render_window::{RenderWindow, RenderWindows},
+        target_alloc::ShareTargetView,
+    },
+    init_render,
+    rhi::{options::RenderOptions, device::RenderDevice},
+};
 use std::{any::TypeId, sync::Arc, time::Instant};
-use pi_render::{rhi::options::RenderOptions, init_render, components::view::{target_alloc::ShareTargetView, render_window::{RenderWindow, RenderWindows}}};
 use wgpu::PresentMode;
 use winit::window::Window;
 
+use pi_3d::{
+    engine::Engine,
+    object::GameObject,
+    systems::{
+        camera_sys::{
+            CameraTransformMatricCacl, FreeCameraProjectionCacl, TargetCameraEffectLocalRotation,
+            TargetCameraViewMatrixCacl,
+        },
+        transform_node_sys::{LocalMatrixCacl, LocalRotationMatrixCacl, WorldMatrixCacl},
+    },
+};
+use pi_async::{
+    prelude::{Mutex, WorkerRuntime},
+    rt::{AsyncRuntime, AsyncRuntimeBuilder},
+};
+use pi_ecs::{
+    entity::Id,
+    prelude::{
+        ArchetypeId, IntoSystem, Query, Res, ResMut, Setup, SingleDispatcher, StageBuilder, System,
+        World,
+    },
+};
 use pi_share::{Share, ShareMutex, ShareRwLock};
-use pi_3d::{systems::{transform_node_sys::{LocalMatrixCacl, WorldMatrixCacl, LocalRotationMatrixCacl}, camera_sys::{TargetCameraEffectLocalRotation, TargetCameraViewMatrixCacl, FreeCameraProjectionCacl, CameraTransformMatricCacl}}, object::GameObject, engine::Engine};
-use pi_async::{rt::{AsyncRuntimeBuilder, AsyncRuntime}, prelude::{WorkerRuntime, Mutex}};
-use pi_ecs::{prelude::{World, IntoSystem, System, Query, ArchetypeId, StageBuilder, Setup, SingleDispatcher, Res, ResMut}, entity::Id};
 
 pub struct EnginShell {
-	pub win: Arc<Window>,
-	pub dispatcher: SingleDispatcher<WorkerRuntime>,
-	pub world: World,
-	pub rt: WorkerRuntime,
-	pub engine: Engine,
+    pub win: Arc<Window>,
+    pub dispatcher: SingleDispatcher<WorkerRuntime>,
+    pub world: World,
+    pub rt: WorkerRuntime,
+    pub engine: Engine,
 }
 
 pub struct DispatchEnd(pub ShareMutex<bool>);
@@ -40,94 +64,95 @@ impl Default for PreFrameTime {
     }
 }
 
-pub fn create_engine(
-    win: &Arc<Window>,
-    _r: f64,
-) -> EnginShell {
-	let size = win.inner_size();
-	let runtime = AsyncRuntimeBuilder::default_worker_thread(
-		None,
-		None,
-		None,
-		None,
-	);
+pub fn create_engine(win: &Arc<Window>, _r: f64) -> EnginShell {
+    let size = win.inner_size();
+    let runtime = AsyncRuntimeBuilder::default_worker_thread(None, None, None, None);
 
-	let mut world = World::new();
+    let mut world = World::new();
 
-	world.insert_resource(DispatchEnd::default());
+    world.insert_resource(DispatchEnd::default());
 
-	let mut world1 = world.clone();
-	let win1 = win.clone();
-	let rt = runtime.clone();
+    let mut world1 = world.clone();
+    let win1 = win.clone();
+    let rt = runtime.clone();
 
-	let mut result: Share<ShareRwLock<Option<(Engine, SingleDispatcher<WorkerRuntime>)>>> = Share::new(ShareRwLock::new(None));
-	let result1 = result.clone();
-    
-	let _ = runtime.spawn(runtime.alloc(), async move {
-		let world = &mut world1;
-		let options = RenderOptions::default();
-		let render_stages = init_render::<Option<ShareTargetView>>(world, options, win1.clone(), rt.clone()).await;
+    let mut result: Share<ShareRwLock<Option<(Engine, SingleDispatcher<WorkerRuntime>)>>> =
+        Share::new(ShareRwLock::new(None));
+    let result1 = result.clone();
 
-		init_data(world, win1);
+    let _ = runtime.spawn(runtime.alloc(), async move {
+        let world = &mut world1;
+        let options = RenderOptions::default();
+        
+        let render_stages = init_render(world, options, win1.clone(), rt.clone()).await;
+        let limits = world.get_resource::<RenderDevice>().unwrap().limits();
+        let min_uniform_buffer_offset_alignment = limits.min_uniform_buffer_offset_alignment;
+        world.insert_resource(pi_render::rhi::dyn_uniform_buffer::DynUniformBuffer::new(
+            Some("DynUniformBuffer".to_string()),
+            min_uniform_buffer_offset_alignment.max(192),
+        ));
 
-		let mut stages = Vec::new();
+        init_data(world, win1);
 
-		let mut first_stage = StageBuilder::new();
-		let first_run = move |mut frame_start_time: ResMut<FrameStartTime>| {
-			frame_start_time.0 = Instant::now();
-		};
-		first_stage.add_node(IntoSystem::system(first_run, world));
-		stages.push(Arc::new(first_stage.build(world)));
+        let mut stages = Vec::new();
 
-		// 初始化gui stage
-		let mut engine = Engine::new(world);
-		let gui_stages = engine.init(0, 0, size.width, size.height);
-		for stage in gui_stages.into_iter() {
-			stages.push(Arc::new(stage.build(world)));
-		}
-		// stages.push(Arc::new(render_stages.extract_stage.build(world)));
-		stages.push(Arc::new(render_stages.prepare_stage.build(world)));
-		stages.push(Arc::new(render_stages.render_stage.build(world)));
+        let mut first_stage = StageBuilder::new();
+        let first_run = move |mut frame_start_time: ResMut<FrameStartTime>| {
+            frame_start_time.0 = Instant::now();
+        };
+        first_stage.add_node(IntoSystem::system(first_run, world));
+        stages.push(Arc::new(first_stage.build(world)));
 
-		let mut last_stage = StageBuilder::new();
+        // 初始化gui stage
+        let mut engine = Engine::new(world);
+        let gui_stages = engine.init(0, 0, size.width, size.height);
+        for stage in gui_stages.into_iter() {
+            stages.push(Arc::new(stage.build(world)));
+        }
+        // stages.push(Arc::new(render_stages.extract_stage.build(world)));
+        stages.push(Arc::new(render_stages.prepare_stage.build(world)));
+        stages.push(Arc::new(render_stages.render_stage.build(world)));
 
-		let last_run = move |end: Res<DispatchEnd>| {
-			let mut l = end.0.lock();
-			// println!("set end true, {:?}, {:p}", *l, &end.0);
-			*l = true;
-		};
-		last_stage.add_node(IntoSystem::system(last_run, world));
-		stages.push(Arc::new(last_stage.build(world)));
+        let mut last_stage = StageBuilder::new();
 
-		let mut dispatcher = SingleDispatcher::new(rt);
-		dispatcher.init(stages, world);
+        let last_run = move |end: Res<DispatchEnd>| {
+            let mut l = end.0.lock();
+            // println!("set end true, {:?}, {:p}", *l, &end.0);
+            *l = true;
+        };
+        last_stage.add_node(IntoSystem::system(last_run, world));
+        stages.push(Arc::new(last_stage.build(world)));
 
-		*result1.write() = Some((engine, dispatcher));
-	});
-	loop {
-		if result.read().is_some() {
-			match Share::try_unwrap(result) {
-				Ok(r) => {
-					let r = r.into_inner().unwrap();
-					let engine = Box::new(EnginShell {
-						win: win.clone(),
-						dispatcher: r.1,
-						world: World::new(),
-						engine: r.0,
-						rt: runtime.clone(),
-					});
-					return Box::into_inner(engine);
-				}
-				Err(r) => result = r,
-			}
-		}
-	}
+        let mut dispatcher = SingleDispatcher::new(rt);
+        dispatcher.init(stages, world);
+
+        *result1.write() = Some((engine, dispatcher));
+    });
+    loop {
+        if result.read().is_some() {
+            match Share::try_unwrap(result) {
+                Ok(r) => {
+                    let r = r.into_inner().unwrap();
+                    let engine = Box::new(EnginShell {
+                        win: win.clone(),
+                        dispatcher: r.1,
+                        world: World::new(),
+                        engine: r.0,
+                        rt: runtime.clone(),
+                    });
+                    return Box::into_inner(engine);
+                }
+                Err(r) => result = r,
+            }
+        }
+    }
 }
 
-fn init_data(world: &mut World, win: Arc<winit::window::Window>) {
-	// 创建 RenderWindow
-	let render_window = RenderWindow::new(win, PresentMode::Mailbox);
-	let render_windows = world.get_resource_mut::<RenderWindows>().unwrap();
-	render_windows.insert(render_window);
+pub fn run_engine(engine: &EnginShell) {}
 
+fn init_data(world: &mut World, win: Arc<winit::window::Window>) {
+    // 创建 RenderWindow
+    let render_window = RenderWindow::new(win, PresentMode::Mailbox);
+    let render_windows = world.get_resource_mut::<RenderWindows>().unwrap();
+    render_windows.insert(render_window);
 }
--- examples/main.rs
+++ examples/main.rs
@@ -1,14 +1,104 @@
-use std::{sync::Arc, any::TypeId};
+#![feature(box_into_inner)]
 
-use pi_3d::{object::GameObject, flags::{SceneID01, SceneCameraID01}, scene::SceneParam, cameras::{camera::{Camera, CameraParam, CameraRenderData}, target_camera::TargetCameraParam, free_camera::FreeCameraParam}, transforms::transform_node::{TransformNode, LocalTransform, GlobalTransform, TransformDirty}, systems::init_stage, materials::default_material::{DefaultMaterialMeta, DefaultMaterialPropertype}, engine::Engine};
-use pi_async::rt::{multi_thread::{MultiTaskRuntimeBuilder, StealableTaskPool}, AsyncRuntime};
-use pi_ecs::prelude::{World, StageBuilder, IntoSystem, SingleDispatcher, Dispatcher, Query};
+mod context;
+use context::{create_engine, EnginShell};
+use pi_3d::{
+    cameras::{
+        camera::{Camera, CameraParam, CameraRenderData},
+        free_camera::FreeCameraParam,
+        target_camera::TargetCameraParam,
+    },
+    engine::Engine,
+    flags::{SceneCameraID01, SceneID01},
+    materials::default_material::{DefaultMaterialMeta, DefaultMaterialPropertype},
+    object::GameObject,
+    scene::SceneParam,
+    systems::init_stage,
+    transforms::transform_node::{GlobalTransform, LocalTransform, TransformDirty, TransformNode},
+};
+use pi_async::rt::{
+    multi_thread::{MultiTaskRuntimeBuilder, StealableTaskPool},
+    AsyncRuntime,
+};
+use pi_ecs::prelude::{Dispatcher, IntoSystem, Query, SingleDispatcher, StageBuilder, World};
+use pi_hal::runtime::MULTI_MEDIA_RUNTIME;
 use pi_render::rhi::dyn_uniform_buffer::DynUniformBuffer;
 use pi_scene_math::{coordiante_system::CoordinateSytem3, Vector3};
-
-// mod context;
+use std::{any::TypeId, sync::Arc, time::{Instant, Duration}};
 
 pub fn main() {
+    env_logger::init();
+
+    let event_loop = winit::event_loop::EventLoop::new();
+    let window = Arc::new(winit::window::Window::new(&event_loop).unwrap());
+
+    let size = window.inner_size();
+
+    let engine = create_engine(&window, 0.0);
+
+    run_loop(engine);
+    
+    run_window_loop(window, event_loop);
+}
+
+fn run_loop(engine: EnginShell) {
+    MULTI_MEDIA_RUNTIME
+        .spawn(MULTI_MEDIA_RUNTIME.alloc(), async move {
+            // example.init(&mut engine, (size.width as usize, size.height as usize)).await;
+
+            let mut pre_frame_time = Instant::now();
+            loop {
+                // 运行
+                // example.render(&mut engine);
+
+                engine.dispatcher.run().await;
+
+                let time = Instant::now();
+                // let _use_time = Instant::now() - pre_frame_time;
+                let time1 = pre_frame_time.clone();
+
+                if time > time1 {
+                    let d = time - time1;
+                    let duration = if d > Duration::from_millis(16) {
+                        Duration::from_millis(0)
+                    } else {
+                        Duration::from_millis(16) - d
+                    };
+                    spin_sleep::sleep(duration);
+                }
+                pre_frame_time = time;
+            }
+        })
+        .unwrap();
+}
+
+fn run_window_loop(window: Arc<winit::window::Window>, event_loop: winit::event_loop::EventLoop<()>) {
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = winit::event_loop::ControlFlow::Wait;
+
+        match event {
+            winit::event::Event::WindowEvent {
+                event: winit::event::WindowEvent::Resized(_size),
+                ..
+            } => {}
+            winit::event::Event::MainEventsCleared => {
+                window.request_redraw();
+            }
+            winit::event::Event::RedrawRequested(_) => {}
+            winit::event::Event::WindowEvent {
+                // 窗口 关闭，退出 循环
+                event: winit::event::WindowEvent::CloseRequested,
+                ..
+            } => {
+                log::info!("RenderExample::clean");
+                *control_flow = winit::event_loop::ControlFlow::Exit
+            }
+            _ => {}
+        }
+    });
+}
+
+pub fn main1() {
     env_logger::init();
 
     let mut world = World::new();
@@ -18,28 +108,27 @@ pub fn main() {
 
     let mut engine = Engine::new(&mut world);
     let stage_builders = engine.init(0, 0, 100, 100);
-    
-	// 创建一个运行时
-	let pool = MultiTaskRuntimeBuilder::<(), StealableTaskPool<()>>::default();
-	// 创建一个运行时
-	let rt = pool.build();
 
+    // 创建一个运行时
+    let pool = MultiTaskRuntimeBuilder::<(), StealableTaskPool<()>>::default();
+    // 创建一个运行时
+    let rt = pool.build();
 
-	// 创建派发器
-	let mut dispatcher = SingleDispatcher::new(rt.clone());
-	let mut stages = Vec::new();
+    // 创建派发器
+    let mut dispatcher = SingleDispatcher::new(rt.clone());
+    let mut stages = Vec::new();
     stage_builders.into_iter().for_each(|stage| {
         stages.push(Arc::new(stage.build(&world)));
     });
-	dispatcher.init(stages, &world);
+    dispatcher.init(stages, &world);
 
     let scene01 = engine.new_scene();
     let node01 = engine.new_transform_node(scene01);
     let camera01 = engine.new_free_camera(scene01);
 
     println!("Run:");
-	// 运行派发器，通常每帧推动
-	dispatcher.run();
+    // 运行派发器，通常每帧推动
+    dispatcher.run();
 
     world.insert_resource(dynbuffer);
 
