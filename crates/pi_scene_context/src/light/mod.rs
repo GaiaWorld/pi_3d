7d2f77e2a72c9baa1032c7e90d31c3984c3091d9
bp0002 <bp950722@outlook.com>
2024-11-01 16:43:32 +0800

    # fix: light limit

--- crates/pi_scene_context/src/light/mod.rs
+++ crates/pi_scene_context/src/light/mod.rs
@@ -34,14 +34,14 @@ impl Plugin for PluginLighting {
         app.configure_set(Update, StageLighting::LightingCommand .in_set(ERunStageChap::D3).after(StageLighting::_LightCreate));
         app.configure_set(Update, StageLighting::LightingUniform .in_set(ERunStageChap::D3).in_set(FrameDataPrepare).after(StageLighting::LightingCommand).after(EStageAnimation::Running).after(StageTransform::TransformCalcMatrix).before(ERunStageChap::Uniform));
 
-        if app.world.contains_resource::<SceneLightLimit>() == false {
+        if app.world.get_resource::<SceneLightLimit>().is_none() {
             app.insert_resource(SceneLightLimit(LightLimitInfo { max_direct_light_count: 8, max_point_light_count: 128, max_spot_light_count: 128, max_hemi_light_count: 8 }));
         }
-        if app.world.contains_resource::<ModelLightLimit>() == false {
+        if app.world.get_resource::<ModelLightLimit>().is_none() {
             app.insert_resource(ModelLightLimit(LightLimitInfo { max_direct_light_count: 4, max_point_light_count: 16, max_spot_light_count: 16, max_hemi_light_count: 4 }));
         }
 
-        if app.world.contains_resource::<SceneShadowLimit>() == false {
+        if app.world.get_resource::<SceneShadowLimit>().is_none() {
             app.insert_resource(SceneShadowLimit(
                 ShadowLimitInfo { max_count: 1, max_width: 1024, max_height: 1024, color_format: ColorFormat::Rgba16Float, depth_stencil_format: DepthStencilFormat::Depth32Float }
             ));
